5fd5121085ff7ceb6f794c6f99053beb1b398968
girishramnani <girishramnani95@gmail.com>
2015-02-08 05:59:58 +0530

    manage command added

--- manage.py
+++ manage.py
@@ -6,6 +6,22 @@ from app import create_app
 from flask.ext.script import Manager
 app = create_app("development")
 manager = Manager(app)
+from app import db
+from app.model import User
 
+@manager.command
+def adduser(email,username,admin=False):
+    from getpass import getpass
+    password=getpass()
+    password2 =getpass(prompt="Confirm: ")
+    if password !=password2:
+        import sys
+        sys.exit("Error : password do not match")
+
+    db.create_all()
+    user = User(email=email,username=username,password=password,is_admin=admin)
+    db.session.add(user)
+    db.session.commit()
+    print("done")
 if __name__ =="__main__":
     manager.run()
